64cab90f53bb3208569492c7fa83ae1b58be8eb0
Hediye Ersan <hediyesnl9@gmail.com>
2025-04-17 17:14:45 +0300

    tailwind sorununu çözer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  safelist: [
+    {
+      pattern: /bg-(red|green|blue|yellow|purple|pink)-(100|200|300|400|500)/,
+    },
+    {
+      pattern: /text-(red|green|blue|yellow|purple|pink)-(100|200|300|400|500)/,
+    },
+  ],
   theme: {
     extend: {},
   },
